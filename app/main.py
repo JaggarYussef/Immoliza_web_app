293e7a07873f64942aa4b8c7efd2634bff1a5957
Lu_cc <luca.var@gmail.com>
2024-02-27 10:47:27 +0100

    main script w dummy predict function

--- app/main.py
+++ app/main.py
@@ -1,12 +1,51 @@
 # Import modules
-from fastapi import FastAPI
-from typing import Union
+from fastapi import FastAPI, HTTPException
+from typing import Optional
+from pydantic import BaseModel
 
+
+# Dummy predict function
+def predict(data):
+    return {"prediction": "prediction"}
+
+
+# class that defines the structure of the input data
+class Property(BaseModel):
+    LivingArea: int
+    TypeOfProperty: str
+    Bedrooms: int
+    PostalCode: int
+    SurfaceOfGood: Optional[int] = None
+    Garden: Optional[bool] = None
+    GardenArea: Optional[int] = None
+    SwimmingPool: Optional[bool] = None
+    Furnished: Optional[bool] = None
+    Openfire: Optional[bool] = None
+    Terrace: Optional[bool] = None
+    NumberOfFacades: Optional[int] = None
+    ConstructionYear: Optional[int] = None
+    StateOfBuilding: Optional[str] = None
+    Kitchen: Optional[str] = None
+
+
+# initialized fastAPI instance
 app = FastAPI()
 
 
+# API endpoint to check if the API is alive
 @app.get("/")
 def health():
     return {"message": "Server is Running"}
 
-print('this is an update inside dev')
+
+# API endpoint to access the prediction
+@app.post("/predict")
+def prediction_calculator(property_data: Property):
+    try:
+        prediction = predict(property_data.dict())
+        return prediction
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+print("this is an update inside dev")
